b01b31cde97ee3e72459756de8fb8b51af3ffdef
qdosgit3 <228497977+qdosgit3@users.noreply.github.com>
2026-02-08 19:19:23 +0000

    Minor refactor

--- alpha_test.jl
+++ alpha_test.jl
@@ -1,28 +1,59 @@
 
 
-function permutation_generator(alpha:: Int64)
+function p_y_given_theta(alpha_len ::Int64, d ::Int64, y ::Int64)
 
-    alpha = zeros(UInt64, 8)
+    alpha = zeros(UInt64, alpha_len)
+
+    alpha_plus_d = zeros(UInt64, alpha_len)
+
+    res = zeros(UInt16, 65536^16)
 
     for i = UInt128(0):2 #UInt128(typemax(UInt128))
 
-        and_bits = UInt128(0xFFFF)
+        alpha = generate_permutation(i, alpha_len)
 
-        for j = 0:7
+        for j = 1:length(alpha)
 
-            println(bitstring(and_bits))
-
-            println()
-            
-            and_bits = and_bits << 16
-
+            alpha_plus_d[j] = alpha[j] + d
 
         end
+
+        res = sum(alpha_plus_d)
 
     end
 
 end
 
 
+function generate_permutation(i ::UInt128, alpha_len ::Int64)
 
-permutation_generator(Int64(8))
+    alpha = zeros(UInt64, alpha_len)
+
+    and_bits = UInt128(0xFFFF)
+
+    a = UInt128(0)
+
+    shift_size = div(128, alpha_len)
+
+    for j = 0:7
+
+        println(bitstring(and_bits))
+
+        println()
+
+        a = and_bits & i
+
+        alpha[j+1] = a >> (j * shift_size)
+        
+        and_bits = and_bits << shift_size
+
+    end
+
+    return alpha
+    
+end
+
+
+generate_permutation(UInt128(1), 8)
+
+#permutation_generator(8, 100)
